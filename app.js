98ea068a4bd2d4e53cd8680f184b4c76fcf03ed2
Artiom <sinigur2006@gmail.com>
2025-02-26 21:57:51 -0500

    Advanced event handling

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const nav = document.querySelector('nav');
-    console.log(nav)
+})
+
+// Creating a Custom Event
+const customEvent = new CustomEvent('customevent', {
+    detail: {
+        name: 'John Doe'
+    }
+})
+
+document.addEventListener('customevent', (event) => {
+    console.log(`Hi: ${event.detail.name}`)
+})
+document.dispatchEvent(customEvent)
+
+//  Using Custom Events in DOM Elements
+const button = document.createElement('button')
+button.textContent = 'Custom Event'
+document.body.appendChild(button)
+
+button.addEventListener('click', (event) => {
+    const customClickElement = new CustomEvent('customclick', {
+        detail: {
+            name: 'Click on Custom Event'
+        }
+    })
+    button.dispatchEvent(customClickElement)
+})
+button.addEventListener('customclick', (event) => {
+    console.log(event.detail.name)
 })
