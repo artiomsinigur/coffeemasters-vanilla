0e149501f0e931651a83a7f8f0c86e095754061f
Artiom <sinigur2006@gmail.com>
2025-03-04 23:30:34 -0500

    Loading menu data

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import { $, $$ } from './helpers.js';
 import API from './services/API.js';
 import Store from './services/Store.js';
+import { loadData } from './services/Menu.js';
+
+window.app = {}
+app.store = Store
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    
+    loadData()
 })
