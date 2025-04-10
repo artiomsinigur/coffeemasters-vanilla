c37ae4af808ec70f45a8901570b5a7118db0506f
Artiom <sinigur2006@gmail.com>
2025-04-09 22:29:03 -0400

    Loading styles into web component

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,14 +1,24 @@
 export class MenuPage extends HTMLElement {
     constructor() {
         super()
+        this.root = this.attachShadow({ mode: 'open' })
 
-        // this.attachShadow({ mode: 'open' })
+        const styles = document.createElement('style')
+        this.root.appendChild(styles)
+
+        // Fetch the CSS file and apply it to the shadow DOM
+        async function fetchMenu() {
+            const request = await fetch('/components/MenuPage.css')
+            const css = await request.text()
+            styles.textContent = css
+        }
+        fetchMenu()
     }
     
     connectedCallback() {
         const template = document.getElementById('menu-page-template')
         const content = template.content.cloneNode(true)
-        this.appendChild(content)
+        this.root.appendChild(content)
     }
 }
 
